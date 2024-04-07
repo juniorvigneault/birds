c19dd54faf1e23e64698d8327943cb25ce902a0a
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2024-04-07 11:57:18 -0400

    Switched frame rate to 25 fps and also changed the detection score threshold to 15%, which allows alot of birdsssss. Runs more smoothly now?? I also put everything into one cavas instead of 2 separateee. Trying to make it a bit cleaner/more performing. Considering adding to this and make this my final project instead?

--- mediapipe/explorations/test_withVideo.js
+++ mediapipe/explorations/test_withVideo.js
@@ -22,7 +22,8 @@ let labels = [];
 let birdsDetected = [];
 // let cutOutBirdImage;
 let allBirdImages = [];
-
+let trailFrames = []; // Array to store previous frames for the trail effect
+let maxTrailFrames = 400; // Maximum number of frames to keep in the trail
 // video variable for footage 
 let birdFootage = {
   p5VideoLayer: undefined,
@@ -30,7 +31,8 @@ let birdFootage = {
   path: 'images/birds2.mp4',
   isRunning: false,
   width: 640,
-  height: 360
+  height: 360,
+  margin: 10
 }
 
 let sketch = new p5(function (p5) {
@@ -41,25 +43,22 @@ let sketch = new p5(function (p5) {
   p5.setup = async function () {
     // create a video element from the video footage for the canvas
     birdFootage.p5VideoLayer = p5.createVideo(birdFootage.path);
-    p5.createCanvas(birdFootage.width, birdFootage.height);
+    p5.createCanvas(birdFootage.width, (birdFootage.height * 2) + birdFootage.margin);
     // initialize bird detection 
     await initializeObjectDetector();
     // initialize bird segmentation
-    await createImageSegmenter();
     birdFootage.p5VideoLayer.loop();
     isDetecting = true;
+    birdFootage.htmlVideoLayer = document.querySelector('video');
+    birdFootage.htmlVideoLayer.muted = true;
 
   }
   // run video and detections and draw rectangles around birds
   p5.draw = function () {
-    p5.background(0);
+    p5.frameRate(25)
+    p5.background(237, 3, 3);
+    // p5.background(0);
     // copy the video stream to the canvas and position it under it
-    birdFootage.htmlVideoLayer = document.querySelector('video');
-    birdFootage.htmlVideoLayer.muted = true;
-    birdFootage.htmlVideoLayer.style.position = 'fixed';
-    birdFootage.htmlVideoLayer.style.top = '0px';
-    birdFootage.htmlVideoLayer.style.zIndex = '-1';
-
     p5.push();
     p5.image(birdFootage.p5VideoLayer, 0, 0, birdFootage.width, birdFootage.height);
     p5.pop();
@@ -86,34 +85,15 @@ let sketch = new p5(function (p5) {
           p5.pop();
         }
         createBirdImages();
+
+        drawDetectedBirds();
+
+        // Draw the trail frames even if there are no detections
+        drawTrailFrames();
       }
     }
-
-    if (birdImageCreated) {
-      // p5.image(cutOutBirdImage, 0, 0);
-    }
-
-    // Display the new p5 Image object
-    if (birdIsSegmented) {
-      // resultBox(p5.width / 2, p5.height / 2, maskImage.width + 20, maskImage.height + 20);
-
-    }
   }
 
-  // when clicking the canvas, play video in a loop
-  p5.mousePressed = function () {}
-  p5.mouseReleased = function () {
-    // isDetecting = false;
-  }
-
-  p5.keyPressed = function () {
-    if (isDetecting && p5.keyCode === 83) {
-      // pause the footage video loop
-      birdFootage.p5VideoLayer.pause();
-      // create an image with the first bird in the array
-      createBirdImage();
-    }
-  }
 
   async function initializeObjectDetector() {
     const vision = await FilesetResolver.forVisionTasks(
@@ -124,135 +104,21 @@ let sketch = new p5(function (p5) {
         modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
         delegate: "GPU"
       },
-      scoreThreshold: 0.5,
+      scoreThreshold: 0.15,
       runningMode: 'VIDEO'
     });
   }
 
-  async function createImageSegmenter() {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-    );
-
-    imageSegmenter = await ImageSegmenter.createFromOptions(vision, {
-      baseOptions: {
-        modelAssetPath: "sharedModels/1.tflite",
-        //"sharedModels/1.tflite"
-      },
-      outputCategoryMask: true,
-      outputConfidenceMasks: false,
-      runningMode: 'IMAGE'
-    });
-
-    imageSegmenter.getLabels();
-  };
-
-  function segmentBirdImage() {
-    if (birdImageCreated) {
-      // imageSegmenter.segment(cutOutBirdImage.canvas, handleSegmentationResults);
-
-    }
-  }
-
-  function handleSegmentationResults(result) {
-    const {
-      width,
-      height
-    } = result.categoryMask;
-
-    // Create a new p5.js image for the mask
-    maskImage = p5.createImage(width, height);
-    maskImage.loadPixels();
-
-    // Access the pixel data from the segmentation mask
-    let imageData = result.categoryMask.containers[0]; // Assuming only one container
-
-    // Access the pixel data from the original image
-    cutOutBirdImage.loadPixels();
-
-    // Iterate through the pixel data and set the mask image pixels
-    for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width; j++) {
-        let index = i * width + j; // Calculate the index in the 1D array
-        let value = imageData[index]; // Get the pixel value
-
-        // Set the pixel color based on the segmentation mask value
-        if (value > 0) {
-          // Copy the color from the original image
-          let colorIndex = (i * width + j) * 4; // Calculate the color index in the 1D array
-          let r = cutOutBirdImage.pixels[colorIndex];
-          let g = cutOutBirdImage.pixels[colorIndex + 1];
-          let b = cutOutBirdImage.pixels[colorIndex + 2];
-          let a = cutOutBirdImage.pixels[colorIndex + 3];
-          maskImage.set(j, i, p5.color(r, g, b, a)); // Set the color with alpha channel
-        } else {
-          // Set transparent pixel
-          maskImage.set(j, i, p5.color(0, 0, 0, 0));
-        }
-      }
-    }
-
-
-    // Update the pixels of the mask image
-    maskImage.updatePixels();
-    birdIsSegmented = true;
-
-    // You can now use the maskImage for further processing or visualization
-  }
 
   function createBirdImages() {
     allBirdImages = []; // Reset the array
     birdsDetected = results.detections;
-
     for (let i = 0; i < birdsDetected.length; i++) {
       let birdImage = birdFootage.p5VideoLayer.get(birdsDetected[i].boundingBox.originX, birdsDetected[i].boundingBox.originY, birdsDetected[i].boundingBox.width, birdsDetected[i].boundingBox.height);
       allBirdImages.push(birdImage);
     }
-
-
     birdImageCreated = true;
-
-    // console.log(birdsDetected.length, allBirdImages.length)
-    // segmentBirdImage();
   };
-
-  function resultBox(x, y, w, h) {
-    p5.push();
-    p5.rectMode(p5.CENTER);
-    p5.fill(255);
-    // p5.noStroke();
-    p5.stroke(0.5);
-    p5.rect(x, y, w, h);
-    p5.pop();
-
-    p5.push();
-    p5.imageMode(p5.CENTER);
-    p5.image(maskImage, x, y);
-    p5.pop();
-  }
-
-}) // end of p5 sketch
-let sketch2 = new p5(function (p5) {
-  let trailFrames = []; // Array to store previous frames for the trail effect
-  let maxTrailFrames = 200; // Maximum number of frames to keep in the trail
-
-  p5.setup = function () {
-    p5.createCanvas(birdFootage.width, birdFootage.height);
-  };
-
-  p5.draw = function () {
-    p5.background(237, 3, 3);
-    if (birdImageCreated) {
-      if (birdsDetected.length > 0) {
-        p5.imageMode(p5.CORNER);
-
-        drawDetectedBirds();
-      }
-    }
-
-    // Draw the trail frames even if there are no detections
-    drawTrailFrames();
-  }
 
   function drawDetectedBirds() {
     if (birdImageCreated) {
@@ -261,7 +127,7 @@ let sketch2 = new p5(function (p5) {
         let box = birdsDetected[i].boundingBox;
 
         // Draw the current frame of detected birds on the main canvas
-        p5.image(bird, box.originX, box.originY, box.width, box.height);
+        p5.image(bird, box.originX, box.originY + birdFootage.height + birdFootage.margin, box.width, box.height);
 
         // Save the current frame in the trailFrames array
         trailFrames.push({
@@ -281,7 +147,7 @@ let sketch2 = new p5(function (p5) {
     // Draw the trail frames
     for (let i = 0; i < trailFrames.length; i++) {
       let frame = trailFrames[i];
-      p5.image(frame.birdImage, frame.box.originX, frame.box.originY, frame.box.width, frame.box.height);
+      p5.image(frame.birdImage, frame.box.originX, frame.box.originY + birdFootage.height + birdFootage.margin, frame.box.width, frame.box.height);
     }
   }
-});
+}) // end of p5 sketch
