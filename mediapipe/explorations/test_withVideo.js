0c6f8da78c571e2d9ce0cde310769df0a99f16cb
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2024-07-11 17:16:45 +0000

    recovered bird poster...

--- mediapipe/explorations/test_withVideo.js
+++ mediapipe/explorations/test_withVideo.js
@@ -2,7 +2,7 @@
 import {
   ObjectDetector,
   ImageSegmenter,
-  FilesetResolver
+  FilesetResolver,
 } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
 
 // mediapipe stuff
@@ -16,7 +16,7 @@ let birdImageCreated = false;
 // let cutOutBirdImage;
 let maskImage;
 let birdIsSegmented = false;
-// // imageSegmentation 
+// // imageSegmentation
 let imageSegmenter;
 let labels = [];
 let birdsDetected = [];
@@ -24,48 +24,58 @@ let birdsDetected = [];
 let allBirdImages = [];
 let trailFrames = []; // Array to store previous frames for the trail effect
 let maxTrailFrames = 400; // Maximum number of frames to keep in the trail
-// video variable for footage 
+// video variable for footage
 let birdFootage = {
   p5VideoLayer: undefined,
   htmlVideoLayer: undefined,
-  path: 'images/birds_insta.mp4',
+  path: "images/small_crowd.mp4",
   isRunning: false,
   width: 640,
   height: 360,
-  margin: 10
-}
+  margin: 10,
+};
 
 let sketch = new p5(function (p5) {
-
   p5.preload = function () {
-    habitusFont = p5.loadFont('assets/fonts/Habitus-Medium.otf')
-  }
+    habitusFont = p5.loadFont("assets/fonts/Habitus-Medium.otf");
+  };
   p5.setup = async function () {
     // create a video element from the video footage for the canvas
     birdFootage.p5VideoLayer = p5.createVideo(birdFootage.path);
-    p5.createCanvas(birdFootage.width, (birdFootage.height * 2) + birdFootage.margin);
-    // initialize bird detection 
+    p5.createCanvas(
+      birdFootage.width,
+      birdFootage.height * 2 + birdFootage.margin
+    );
+    // initialize bird detection
     await initializeObjectDetector();
     // initialize bird segmentation
     birdFootage.p5VideoLayer.loop();
     isDetecting = true;
-    birdFootage.htmlVideoLayer = document.querySelector('video');
+    birdFootage.htmlVideoLayer = document.querySelector("video");
     birdFootage.htmlVideoLayer.muted = true;
-
-  }
+  };
   // run video and detections and draw rectangles around birds
   p5.draw = function () {
-    p5.frameRate(25)
+    p5.frameRate(25);
     p5.background(237, 3, 3);
     // p5.background(0);
     // copy the video stream to the canvas and position it under it
     p5.push();
-    p5.image(birdFootage.p5VideoLayer, 0, 0, birdFootage.width, birdFootage.height);
+    p5.image(
+      birdFootage.p5VideoLayer,
+      0,
+      0,
+      birdFootage.width,
+      birdFootage.height
+    );
     p5.pop();
     // if the the model is initialized, run detection on video and draw rectangles around birds
     if (objectDetector && isDetecting) {
       // put the detections of the video in results
-      results = objectDetector.detectForVideo(birdFootage.htmlVideoLayer, p5.millis());
+      results = objectDetector.detectForVideo(
+        birdFootage.htmlVideoLayer,
+        p5.millis()
+      );
 
       birdsDetected = results.detections;
       // console.log(birdsDetected.categories)
@@ -77,11 +87,15 @@ let sketch = new p5(function (p5) {
           // p5.fill(255, 255, 255, 0)
           // p5.stroke(230, 0, 0)
           // p5.rect(box.originX, box.originY, box.width, box.height)
-          p5.fill(0)
-          p5.textFont(habitusFont)
-          p5.text(Math.floor(birdsDetected[i].categories[0].score * 100) % 100, box.originX, box.originY);
           p5.fill(0);
-          p5.ellipse(box.originX - 10, box.originY + 5, 5, 5)
+          p5.textFont(habitusFont);
+          p5.text(
+            Math.floor(birdsDetected[i].categories[0].score * 100) % 100,
+            box.originX,
+            box.originY
+          );
+          p5.fill(0);
+          p5.ellipse(box.originX - 10, box.originY + 5, 5, 5);
           p5.pop();
         }
         createBirdImages();
@@ -92,8 +106,7 @@ let sketch = new p5(function (p5) {
         drawTrailFrames();
       }
     }
-  }
-
+  };
 
   async function initializeObjectDetector() {
     const vision = await FilesetResolver.forVisionTasks(
@@ -102,23 +115,27 @@ let sketch = new p5(function (p5) {
     objectDetector = await ObjectDetector.createFromOptions(vision, {
       baseOptions: {
         modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
-        delegate: "GPU"
+        delegate: "GPU",
       },
       scoreThreshold: 0.15,
-      runningMode: 'VIDEO'
+      runningMode: "VIDEO",
     });
   }
-
 
   function createBirdImages() {
     allBirdImages = []; // Reset the array
     birdsDetected = results.detections;
     for (let i = 0; i < birdsDetected.length; i++) {
-      let birdImage = birdFootage.p5VideoLayer.get(birdsDetected[i].boundingBox.originX, birdsDetected[i].boundingBox.originY, birdsDetected[i].boundingBox.width, birdsDetected[i].boundingBox.height);
+      let birdImage = birdFootage.p5VideoLayer.get(
+        birdsDetected[i].boundingBox.originX,
+        birdsDetected[i].boundingBox.originY,
+        birdsDetected[i].boundingBox.width,
+        birdsDetected[i].boundingBox.height
+      );
       allBirdImages.push(birdImage);
     }
     birdImageCreated = true;
-  };
+  }
 
   function drawDetectedBirds() {
     if (birdImageCreated) {
@@ -127,12 +144,18 @@ let sketch = new p5(function (p5) {
         let box = birdsDetected[i].boundingBox;
 
         // Draw the current frame of detected birds on the main canvas
-        p5.image(bird, box.originX, box.originY + birdFootage.height + birdFootage.margin, box.width, box.height);
+        p5.image(
+          bird,
+          box.originX,
+          box.originY + birdFootage.height + birdFootage.margin,
+          box.width,
+          box.height
+        );
 
         // Save the current frame in the trailFrames array
         trailFrames.push({
           birdImage: bird,
-          box: box
+          box: box,
         });
 
         // Limit the number of frames in the trailFrames array
@@ -147,7 +170,13 @@ let sketch = new p5(function (p5) {
     // Draw the trail frames
     for (let i = 0; i < trailFrames.length; i++) {
       let frame = trailFrames[i];
-      p5.image(frame.birdImage, frame.box.originX, frame.box.originY + birdFootage.height + birdFootage.margin, frame.box.width, frame.box.height);
+      p5.image(
+        frame.birdImage,
+        frame.box.originX,
+        frame.box.originY + birdFootage.height + birdFootage.margin,
+        frame.box.width,
+        frame.box.height
+      );
     }
   }
-}) // end of p5 sketch
+}); // end of p5 sketch
