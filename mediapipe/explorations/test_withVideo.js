6dcac2c4ab0444f0cf3b93662689e2d7f7495ab0
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2024-07-29 11:14:30 +0000

    Created residency version

--- mediapipe/explorations/test_withVideo.js
+++ mediapipe/explorations/test_withVideo.js
@@ -1,182 +1,308 @@
-// import mediapipe detection stuff
+// Import Mediapipe detection stuff
 import {
-  ObjectDetector,
-  ImageSegmenter,
   FilesetResolver,
-} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
+  HandLandmarker,
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 
-// mediapipe stuff
-let objectDetector;
-let runningMode = "VIDEO";
-let habitusFont;
-// store the results of the model
-let results;
-let isDetecting = false;
-let birdImageCreated = false;
-// let cutOutBirdImage;
-let maskImage;
-let birdIsSegmented = false;
-// // imageSegmentation
-let imageSegmenter;
-let labels = [];
-let birdsDetected = [];
-// let cutOutBirdImage;
-let allBirdImages = [];
-let trailFrames = []; // Array to store previous frames for the trail effect
-let maxTrailFrames = 400; // Maximum number of frames to keep in the trail
-// video variable for footage
-let birdFootage = {
-  p5VideoLayer: undefined,
-  htmlVideoLayer: undefined,
-  path: "images/small_crowd.mp4",
-  isRunning: false,
-  width: 640,
-  height: 360,
-  margin: 10,
-};
+// Matter.js components
+const {
+  Engine,
+  Events,
+  Runner,
+  Bodies,
+  Vector,
+  Body,
+  Composite,
+  World,
+  Composites,
+  MouseConstraint,
+  Mouse,
+  Constraint,
+} = Matter;
 
-let sketch = new p5(function (p5) {
+let engine, world, mouseConstraint, streamCanvas, handsCanvas;
+let particles = [],
+  trailFrames = [],
+  handsDetected = [];
+let handLandmarker, habitusFont, results;
+let isDetecting = false,
+  handImageCreated = false;
+let landmarkPixelSize,
+  handFootage = {
+    p5VideoLayer: undefined,
+    htmlVideoLayer: undefined,
+    path: "images/small_crowd.mp4",
+    isRunning: false,
+    width: 640,
+    height: 480,
+    margin: 10,
+  };
+let allHandImages = [];
+
+const maxTrailFrames = 1000; // Maximum number of frames to keep in the trail
+
+// let stream = new p5((s) => {
+//   s.setup = function () {
+//     streamCanvas = s.createCanvas(handFootage.width, handFootage.height);
+//     streamCanvas.parent("stream-canvas");
+//     createEngine();
+//     // setInterval(makeStream, 100); // Lower the frequency if necessary
+//   };
+
+//   s.draw = function () {
+//     s.background(0);
+//     s.clear(streamCanvas);
+//     drawParticles(s);
+//     Engine.update(engine);
+//     checkHandInteractions(s);
+//     makeStream();
+//   };
+
+//   function createEngine() {
+//     engine = Engine.create();
+//     world = engine.world;
+//     Runner.run(engine);
+
+//     const mouse = Mouse.create(document.querySelector("#particles-canvas"));
+//     mouseConstraint = MouseConstraint.create(engine, { mouse });
+//     World.add(world, mouseConstraint);
+//   }
+
+//   function makeStream() {
+//     // Get the current seconds
+//     const currentTime = new Date();
+//     const currentSeconds = currentTime.getSeconds();
+
+//     const randomSize = s.random(8, 12);
+//     const group = Body.nextGroup(true);
+
+//     const particle = Bodies.circle(s.width / 2, -100, randomSize, {
+//       friction: 0,
+//       density: 0.5,
+//       restitution: 0.7,
+//       collisionFilter: { group },
+//     });
+
+//     World.add(world, particle);
+//     particles.push(particle);
+
+//     const randomFrameCount = s.random(10, 20);
+//     if (s.frameCount % 90 > randomFrameCount) {
+//       const randomSmall = s.random(5, 8);
+//       const smallParticle = Bodies.circle(s.width / 2, -90, randomSmall, {
+//         friction: 0,
+//         restitution: 0.7,
+//         collisionFilter: { group },
+//       });
+
+//       World.add(world, smallParticle);
+//       particles.push(smallParticle);
+//     }
+//   }
+
+//   function drawParticles(s) {
+//     for (let i = 0; i < particles.length; i++) {
+//       const particle = particles[i];
+//       s.push();
+//       s.fill(0);
+//       s.noStroke();
+//       s.ellipseMode(s.CENTER);
+//       s.ellipse(
+//         particle.position.x,
+//         particle.position.y,
+//         particle.circleRadius * 2
+//       );
+//       // s.text("10", particle.position.x, particle.position.y);
+//       s.pop();
+
+//       if (particle.position.y > 600) {
+//         World.remove(world, particle);
+//         particles.splice(i, 1);
+//         i--;
+//       }
+//     }
+//   }
+
+//   function checkHandInteractions(s) {
+//     if (handsDetected.length > 0) {
+//       for (const hand of handsDetected) {
+//         for (const landmark of hand) {
+//           for (const particle of particles) {
+//             const distance = s.dist(
+//               landmark.x * handFootage.width,
+//               landmark.y * handFootage.height,
+//               particle.position.x,
+//               particle.position.y
+//             );
+
+//             if (distance < 50) {
+//               const forceDirection = Vector.sub(
+//                 { x: particle.position.x, y: particle.position.y },
+//                 {
+//                   x: landmark.x * handFootage.width,
+//                   y: landmark.y * handFootage.height,
+//                 }
+//               );
+//               const force = Vector.mult(Vector.normalise(forceDirection), 5);
+//               Body.applyForce(particle, particle.position, force);
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// });
+
+let sketch = new p5((p5) => {
   p5.preload = function () {
     habitusFont = p5.loadFont("assets/fonts/Habitus-Medium.otf");
   };
+
   p5.setup = async function () {
-    // create a video element from the video footage for the canvas
-    birdFootage.p5VideoLayer = p5.createVideo(birdFootage.path);
-    p5.createCanvas(
-      birdFootage.width,
-      birdFootage.height * 2 + birdFootage.margin
-    );
-    // initialize bird detection
-    await initializeObjectDetector();
-    // initialize bird segmentation
-    birdFootage.p5VideoLayer.loop();
+    handFootage.p5VideoLayer = p5.createCapture(p5.VIDEO);
+    handsCanvas = p5.createCanvas(handFootage.width, handFootage.height);
+    handsCanvas.parent("hands-canvas");
+    handFootage.p5VideoLayer.hide();
+    await initializeHandDetector();
+    handFootage.p5VideoLayer.loop();
     isDetecting = true;
-    birdFootage.htmlVideoLayer = document.querySelector("video");
-    birdFootage.htmlVideoLayer.muted = true;
-  };
-  // run video and detections and draw rectangles around birds
-  p5.draw = function () {
-    p5.frameRate(25);
-    p5.background(237, 3, 3);
-    // p5.background(0);
-    // copy the video stream to the canvas and position it under it
-    p5.push();
-    p5.image(
-      birdFootage.p5VideoLayer,
-      0,
-      0,
-      birdFootage.width,
-      birdFootage.height
-    );
-    p5.pop();
-    // if the the model is initialized, run detection on video and draw rectangles around birds
-    if (objectDetector && isDetecting) {
-      // put the detections of the video in results
-      results = objectDetector.detectForVideo(
-        birdFootage.htmlVideoLayer,
-        p5.millis()
-      );
-
-      birdsDetected = results.detections;
-      // console.log(birdsDetected.categories)
-      // draw a rect around each bird
-      if (birdsDetected.length > 0) {
-        for (let i = 0; i < birdsDetected.length; i++) {
-          p5.push();
-          let box = birdsDetected[i].boundingBox;
-          // p5.fill(255, 255, 255, 0)
-          // p5.stroke(230, 0, 0)
-          // p5.rect(box.originX, box.originY, box.width, box.height)
-          p5.fill(0);
-          p5.textFont(habitusFont);
-          p5.text(
-            Math.floor(birdsDetected[i].categories[0].score * 100) % 100,
-            box.originX,
-            box.originY
-          );
-          p5.fill(0);
-          p5.ellipse(box.originX - 10, box.originY + 5, 5, 5);
-          p5.pop();
-        }
-        createBirdImages();
-
-        drawDetectedBirds();
-
-        // Draw the trail frames even if there are no detections
-        drawTrailFrames();
-      }
-    }
+    handFootage.htmlVideoLayer = document.querySelector("video");
+    handFootage.htmlVideoLayer.muted = true;
   };
 
-  async function initializeObjectDetector() {
+  async function initializeHandDetector() {
     const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
     );
-    objectDetector = await ObjectDetector.createFromOptions(vision, {
+    handLandmarker = await HandLandmarker.createFromOptions(vision, {
       baseOptions: {
-        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
+        modelAssetPath:
+          "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
         delegate: "GPU",
       },
-      scoreThreshold: 0.15,
       runningMode: "VIDEO",
+      numHands: 10,
     });
   }
 
-  function createBirdImages() {
-    allBirdImages = []; // Reset the array
-    birdsDetected = results.detections;
-    for (let i = 0; i < birdsDetected.length; i++) {
-      let birdImage = birdFootage.p5VideoLayer.get(
-        birdsDetected[i].boundingBox.originX,
-        birdsDetected[i].boundingBox.originY,
-        birdsDetected[i].boundingBox.width,
-        birdsDetected[i].boundingBox.height
-      );
-      allBirdImages.push(birdImage);
+  p5.draw = function () {
+    // p5.background(255);
+    // p5.clear(handsCanvas);
+
+    if (!handFootage.p5VideoLayer) {
+      console.error("handFootage or handFootage.p5VideoLayer is undefined");
+      return;
     }
-    birdImageCreated = true;
+
+    p5.translate(handFootage.width, 0);
+    p5.scale(-1, 1);
+
+    const videoBuffer = p5.createGraphics(
+      handFootage.width * 2,
+      handFootage.height * 2
+    );
+    videoBuffer.image(
+      handFootage.p5VideoLayer,
+      0,
+      0,
+      handFootage.width,
+      handFootage.height
+    );
+
+    const maskBuffer = p5.createGraphics(handFootage.width, handFootage.height);
+    maskBuffer.clear();
+    maskBuffer.fill(255);
+    maskBuffer.noStroke();
+
+    if (
+      handLandmarker &&
+      isDetecting &&
+      handFootage.p5VideoLayer.elt.readyState >= 2
+    ) {
+      results = handLandmarker.detectForVideo(
+        handFootage.p5VideoLayer.elt,
+        p5.millis()
+      );
+      handsDetected = results.landmarks;
+      // drawTrailFrames(p5);
+      //
+      if (handsDetected.length > 0) {
+        for (const hand of handsDetected) {
+          // maskBuffer.beginShape();
+          for (const point of hand) {
+            maskBuffer.vertex(
+              point.x * handFootage.width,
+              point.y * handFootage.height
+            );
+            createLandmarkPixels(p5, point);
+            drawLandmarkPixels(p5, point);
+          }
+          // maskBuffer.endShape(p5.CLOSE);
+        }
+
+        // const videoImage = videoBuffer.get();
+        // videoImage.mask(maskBuffer);
+        // p5.image(videoImage, 0, 0, handFootage.width * 2, handFootage.height * 2);
+      }
+    }
+  };
+
+  function createLandmarkPixels(p5, point) {
+    allHandImages = [];
+    landmarkPixelSize = p5.map(point.z, -10, 5, 100, 0);
+
+    const handImage = handFootage.p5VideoLayer.get(
+      point.x * handFootage.width,
+      point.y * handFootage.height,
+      landmarkPixelSize,
+      landmarkPixelSize
+    );
+
+    allHandImages.push(handImage);
+    handImageCreated = true;
   }
 
-  function drawDetectedBirds() {
-    if (birdImageCreated) {
-      for (let i = 0; i < allBirdImages.length; i++) {
-        let bird = allBirdImages[i];
-        let box = birdsDetected[i].boundingBox;
-
-        // Draw the current frame of detected birds on the main canvas
-        p5.image(
-          bird,
-          box.originX,
-          box.originY + birdFootage.height + birdFootage.margin,
-          box.width,
-          box.height
-        );
-
-        // Save the current frame in the trailFrames array
+  function drawLandmarkPixels(p5, point) {
+    if (handImageCreated) {
+      for (const handImage of allHandImages) {
         trailFrames.push({
-          birdImage: bird,
-          box: box,
+          handImage,
+          x: point.x * handFootage.width,
+          y: point.y * handFootage.height,
+          size: landmarkPixelSize,
         });
 
-        // Limit the number of frames in the trailFrames array
+        p5.image(
+          handImage,
+          point.x * handFootage.width,
+          point.y * handFootage.height,
+          landmarkPixelSize
+        );
+
         if (trailFrames.length > maxTrailFrames) {
-          trailFrames.shift(); // Remove the oldest frame if the array exceeds the maximum number of frames
+          trailFrames.shift();
         }
       }
     }
   }
 
-  function drawTrailFrames() {
-    // Draw the trail frames
-    for (let i = 0; i < trailFrames.length; i++) {
-      let frame = trailFrames[i];
-      p5.image(
-        frame.birdImage,
-        frame.box.originX,
-        frame.box.originY + birdFootage.height + birdFootage.margin,
-        frame.box.width,
-        frame.box.height
-      );
+  function drawTrailFrames(p5) {
+    for (const frame of trailFrames) {
+      if (frame.handImage) {
+        p5.image(frame.handImage, frame.x, frame.y, frame.size, frame.size);
+      } else {
+        console.error("Invalid frame image");
+      }
     }
   }
-}); // end of p5 sketch
+});
+
+function fillHsluv(h, s, l, sketch) {
+  const rgb = hsluv.hsluvToRgb([h, s, l]);
+  sketch.fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+function strokeHsluv(h, s, l, sketch) {
+  const rgb = hsluv.hsluvToRgb([h, s, l]);
+  sketch.p5.stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
