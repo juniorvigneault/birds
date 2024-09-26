00992634562ddf0432947498ce2ec5d65b31ae60
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2024-09-26 10:29:56 -0400

    test

--- mediapipe/explorations/test_withVideo.js
+++ mediapipe/explorations/test_withVideo.js
@@ -19,7 +19,23 @@ const {
   Mouse,
   Constraint,
 } = Matter;
-
+let cloth;
+let img;
+const CATEGORY_PENIS = 0x0001;
+const CATEGORY_CIRCLE_PARTICLE = 0x0002;
+const CATEGORY_RECTANGLE = 0x0004;
+const CATEGORY_MOUSE = 0x0008;
+let imgLoaded = false;
+let clothOptions = {
+  x: 0,
+  y: 0,
+  col: 70,
+  row: 20,
+  colGap: 2,
+  rowGap: 2,
+  // crossBrace: true,
+  particleRad: 7,
+};
 let engine, world, mouseConstraint, streamCanvas, handsCanvas;
 let particles = [],
   trailFrames = [],
@@ -39,7 +55,7 @@ let landmarkPixelSize,
   };
 let allHandImages = [];
 
-const maxTrailFrames = 1000; // Maximum number of frames to keep in the trail
+const maxTrailFrames = 500; // Maximum number of frames to keep in the trail
 
 // let stream = new p5((s) => {
 //   s.setup = function () {
@@ -58,15 +74,21 @@ const maxTrailFrames = 1000; // Maximum number of frames to keep in the trail
 //     makeStream();
 //   };
 
-//   function createEngine() {
-//     engine = Engine.create();
-//     world = engine.world;
-//     Runner.run(engine);
+function createEngine() {
+  engine = Engine.create();
+  world = engine.world;
+  Runner.run(engine);
+  let mouse = Mouse.create(document.querySelector("#p5js-canvas")),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        // stiffness: 0.0,
+      },
+      collisionFilter: { category: CATEGORY_MOUSE },
+    });
 
-//     const mouse = Mouse.create(document.querySelector("#particles-canvas"));
-//     mouseConstraint = MouseConstraint.create(engine, { mouse });
-//     World.add(world, mouseConstraint);
-//   }
+  World.add(world, mouseConstraint);
+}
 
 //   function makeStream() {
 //     // Get the current seconds
@@ -152,15 +174,86 @@ const maxTrailFrames = 1000; // Maximum number of frames to keep in the trail
 //     }
 //   }
 // });
+function addCloth(p) {
+  cloth = createCloth(
+    p.width / 2,
+    p.height / 2,
+    clothOptions.col,
+    clothOptions.row,
+    clothOptions.colGap,
+    clothOptions.rowGap,
+    clothOptions.crossBrace,
+    clothOptions.particleRad
+  );
+
+  Composite.add(world, cloth);
+}
+
+function createCloth(
+  xx,
+  yy,
+  columns,
+  rows,
+  columnGap,
+  rowGap,
+  crossBrace,
+  particleRadius,
+  particleOptions,
+  constraintOptions
+) {
+  let group = Body.nextGroup(true);
+  particleOptions = Matter.Common.extend(
+    {
+      inertia: Infinity,
+      friction: 0,
+      restitution: 0,
+      collisionFilter: {
+        group: group,
+      },
+      render: { visible: false },
+    },
+    particleOptions
+  );
+
+  constraintOptions = Matter.Common.extend(
+    {
+      stiffness: 1,
+      // render: { type: "line", anchors: false },
+    },
+    constraintOptions
+  );
+
+  let cloth = Composites.stack(
+    xx,
+    yy,
+    columns,
+    rows,
+    columnGap,
+    rowGap,
+    function (x, y) {
+      return Bodies.circle(x, y, particleRadius, particleOptions);
+    }
+  );
+
+  Composites.mesh(cloth, columns, rows, crossBrace, constraintOptions);
+  cloth.label = "Cloth Body";
+
+  return cloth;
+}
 
 let sketch = new p5((p5) => {
   p5.preload = function () {
     habitusFont = p5.loadFont("assets/fonts/Habitus-Medium.otf");
+    img = p5.loadImage("/images/materiality.png");
   };
 
   p5.setup = async function () {
     handFootage.p5VideoLayer = p5.createCapture(p5.VIDEO);
-    handsCanvas = p5.createCanvas(handFootage.width, handFootage.height);
+    handsCanvas = p5.createCanvas(
+      handFootage.width,
+      handFootage.height,
+      p5.WEBGL
+    );
     handsCanvas.parent("hands-canvas");
     handFootage.p5VideoLayer.hide();
     await initializeHandDetector();
@@ -168,6 +261,11 @@ let sketch = new p5((p5) => {
     isDetecting = true;
     handFootage.htmlVideoLayer = document.querySelector("video");
     handFootage.htmlVideoLayer.muted = true;
+    createEngine();
+    addCloth(p5);
+
+    img.loadPixels();
+    imgLoaded = true;
   };
 
   async function initializeHandDetector() {
@@ -186,7 +284,7 @@ let sketch = new p5((p5) => {
   }
 
   p5.draw = function () {
-    // p5.background(255);
+    p5.background(0);
     // p5.clear(handsCanvas);
 
     if (!handFootage.p5VideoLayer) {
@@ -201,6 +299,10 @@ let sketch = new p5((p5) => {
       handFootage.width * 2,
       handFootage.height * 2
     );
+    p5.translate(handFootage.width, 0);
+    //then scale it by -1 in the x-axis
+    //to flip the image
+    p5.scale(-1, 1);
     videoBuffer.image(
       handFootage.p5VideoLayer,
       0,
@@ -226,7 +328,10 @@ let sketch = new p5((p5) => {
       handsDetected = results.landmarks;
       // drawTrailFrames(p5);
       //
-      if (handsDetected.length > 0) {
+      // console.log(handsDetected);
+
+      if (handsDetected.length > 1) {
+        let item = 0;
         for (const hand of handsDetected) {
           // maskBuffer.beginShape();
           for (const point of hand) {
@@ -237,15 +342,271 @@ let sketch = new p5((p5) => {
             createLandmarkPixels(p5, point);
             drawLandmarkPixels(p5, point);
           }
+          // 4,8,12,16,20
           // maskBuffer.endShape(p5.CLOSE);
         }
+
+        // let point = handsDetected[0].
+        // console.log(handsDetected);
 
         // const videoImage = videoBuffer.get();
         // videoImage.mask(maskBuffer);
         // p5.image(videoImage, 0, 0, handFootage.width * 2, handFootage.height * 2);
+        // letters(0);
+        // letters2(1);
       }
     }
+
+    if (imgLoaded) {
+      // renderClothWithImage(cloth, clothOptions.col, clothOptions.row, p5);
+    }
   };
+  function renderClothWithImage(cloth, columns, rows, p) {
+    // Set texture mode to make it consistent with 2D mode
+    p.textureMode(p.NORMAL);
+
+    // Apply texture to the cloth
+    p.texture(img);
+    p.translate(-p.width / 2, -p.height / 2);
+
+    // Iterate over each cloth particle
+    for (let y = 0; y < rows - 1; y++) {
+      for (let x = 0; x < columns - 1; x++) {
+        let index = x + y * columns;
+        let nextIndex = index + 1;
+        let belowIndex = index + columns;
+        let belowNextIndex = belowIndex + 1;
+
+        // Calculate texture coordinates
+        let u1 = x / (columns - 1);
+        let v1 = y / (rows - 1);
+        let u2 = (x + 1) / (columns - 1);
+        let v2 = (y + 1) / (rows - 1);
+        p.noStroke();
+        // Draw the cloth square with texture
+        p.beginShape(p.QUADS);
+        p.texture(img);
+        p.vertex(
+          cloth.bodies[index].position.x,
+          cloth.bodies[index].position.y,
+          u1,
+          v1
+        );
+        p.vertex(
+          cloth.bodies[nextIndex].position.x,
+          cloth.bodies[nextIndex].position.y,
+          u2,
+          v1
+        );
+        p.vertex(
+          cloth.bodies[belowNextIndex].position.x,
+          cloth.bodies[belowNextIndex].position.y,
+          u2,
+          v2
+        );
+        p.vertex(
+          cloth.bodies[belowIndex].position.x,
+          cloth.bodies[belowIndex].position.y,
+          u1,
+          v2
+        );
+        p.endShape(p.CLOSE);
+      }
+    }
+  }
+  function letters(hand) {
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][4].x * handFootage.width,
+      handsDetected[hand][4].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "A",
+      handsDetected[hand][4].x * handFootage.width,
+      handsDetected[hand][4].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][8].x * handFootage.width,
+      handsDetected[hand][8].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "S",
+      handsDetected[hand][8].x * handFootage.width,
+      handsDetected[hand][8].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][12].x * handFootage.width,
+      handsDetected[hand][12].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "M",
+      handsDetected[hand][12].x * handFootage.width,
+      handsDetected[hand][12].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][16].x * handFootage.width,
+      handsDetected[hand][16].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "I",
+      handsDetected[hand][16].x * handFootage.width,
+      handsDetected[hand][16].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][20].x * handFootage.width,
+      handsDetected[hand][20].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "S",
+      handsDetected[hand][20].x * handFootage.width,
+      handsDetected[hand][20].y * handFootage.height + 10
+    );
+    p5.pop();
+  }
+
+  function letters2(hand) {
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][4].x * handFootage.width,
+      handsDetected[hand][4].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "L",
+      handsDetected[hand][4].x * handFootage.width,
+      handsDetected[hand][4].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][8].x * handFootage.width,
+      handsDetected[hand][8].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "A",
+      handsDetected[hand][8].x * handFootage.width,
+      handsDetected[hand][8].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][12].x * handFootage.width,
+      handsDetected[hand][12].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "B",
+      handsDetected[hand][12].x * handFootage.width,
+      handsDetected[hand][12].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][16].x * handFootage.width,
+      handsDetected[hand][16].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "I",
+      handsDetected[hand][16].x * handFootage.width,
+      handsDetected[hand][16].y * handFootage.height + 10
+    );
+    p5.pop();
+
+    p5.push();
+    p5.ellipseMode(p5.CENTER);
+    p5.ellipse(
+      handsDetected[hand][20].x * handFootage.width,
+      handsDetected[hand][20].y * handFootage.height,
+      40,
+      40
+    );
+    p5.fill(255);
+    p5.textAlign(p5.CENTER);
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(
+      "M",
+      handsDetected[hand][20].x * handFootage.width,
+      handsDetected[hand][20].y * handFootage.height + 10
+    );
+    p5.pop();
+  }
 
   function createLandmarkPixels(p5, point) {
     allHandImages = [];
